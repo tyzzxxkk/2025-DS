3a4ef9d70b13573d82d1fa2c9e03b4e8035b88ae
태연 <s2401@e-mirim.hs.kr>
2025-07-09 04:08:12 +0900

    commit

--- DoubleLinkedList.mjs
+++ DoubleLinkedList.mjs
@@ -1,129 +1,141 @@
+// 노드 클래스: 각각의 요소를 담는 단위
 class Node {
-    constructor(data, next=null, prev=null) {
-        this.data = data;
-        this.next = next;
-        this.prev = prev;
+    constructor(data, next = null, prev = null) {
+        this.data = data; // 저장할 데이터
+        this.next = next; // 다음 노드를 가리키는 포인터
+        this.prev = prev; // 이전 노드를 가리키는 포인터
     }
 }
 
 class DoubleLinkedList {
     constructor() {
-        this.head = null; //삽입
-        this.tail = nill; //삭제
-        this.count = 0;
+        this.head = null; // 맨 앞 노드
+        this.tail = null; // 맨 끝 노드
+        this.count = 0;   // 노드 개수
     }
 
+    // 지정된 위치에 노드를 삽입
     insertAt(index, data) {
-        if(index > this.count || index < 0) {
+        if (index > this.count || index < 0) {
             throw new Error("범위를 넘어갔습니다.");
         }
 
-        let newNode = new Node(data);
+        const newNode = new Node(data);
 
-        if(index == 0) {
-            newNode = this.head;
-            if(this.head != null) {
+        if (index === 0) {
+            // 맨 앞에 삽입
+            newNode.next = this.head;
+            if (this.head !== null) {
                 this.head.prev = newNode;
+            } else {
+                // 리스트가 비어있으면 tail도 새 노드로 설정
+                this.tail = newNode;
             }
             this.head = newNode;
-        } 
-
-        else if(index === this.count) {
-            newNode.next = null;
+        } else if (index === this.count) {
+            // 맨 뒤에 삽입 (insertLast와 동일)
             newNode.prev = this.tail;
-            newNode.tail.next = newNode; 
-        }
-
-        else {
+            if (this.tail !== null) {
+                this.tail.next = newNode;
+            } else {
+                // 리스트가 비어있으면 head도 새 노드로 설정
+                this.head = newNode;
+            }
+            this.tail = newNode;
+        } else {
+            // 중간에 삽입
             let currentNode = this.head;
-            for(let i = 0; i < index-1; i++) {
+            for (let i = 0; i < index; i++) {
                 currentNode = currentNode.next;
             }
-            newNode = currentNode.next;
-            newNode.prev = currentNode;
-            currentNode.next = newNode;
-            newNode.next.prev = newNode;
-        }
-
-        if(newNode.next == null) {
-            this.tail = newNode;
+            newNode.prev = currentNode.prev;
+            newNode.next = currentNode;
+            if (currentNode.prev) {
+                currentNode.prev.next = newNode;
+            }
+            currentNode.prev = newNode;
         }
 
         this.count++;
     }
 
-    printAll() {
-        let currentNode = this.head;
-        let text = "[";
-
-        while(currentNode != null) {
-            console.log(currentNode.data);
-            currentNode - currentNode.next;
-
-            if(currentNode != null) {
-                text += ", ";
-            }
-        }
-    }
-
+    // 맨 뒤에 노드를 삽입
     insertLast(data) {
-        this.insertAt()
+        this.insertAt(this.count, data);
     }
 
+    // 지정한 위치의 노드를 삭제하고 반환
     deleteAt(index) {
-        if(index > this.count || index < 0) {
+        if (index >= this.count || index < 0) {
             throw new Error("범위를 넘어갔습니다.");
         }
 
-        let currentNode = this.head;
+        let deleteNode;
 
-        if(index==0) {
-            let deleteNode = this.head;
-
-            if(this.head.next == null) {
+        if (index === 0) {
+            // 맨 앞 노드 삭제
+            deleteNode = this.head;
+            if (this.head.next === null) {
+                // 노드가 하나뿐인 경우
                 this.head = null;
                 this.tail = null;
-            }
-            else {
+            } else {
                 this.head = this.head.next;
                 this.head.prev = null;
             }
-            this.count--;
-            return deleteNode;
-        } 
-        else if(index == this.count-1) {
-            let deleteNode = this.tail;
-            this.tail.prev.next = null;
+        } else if (index === this.count - 1) {
+            // 맨 끝 노드 삭제
+            deleteNode = this.tail;
             this.tail = this.tail.prev;
-            this.count--;
-            return deleteNode;
+            if (this.tail) {
+                this.tail.next = null;
+            }
+        } else {
+            // 중간 노드 삭제
+            let currentNode = this.head;
+            for (let i = 0; i < index; i++) {
+                currentNode = currentNode.next;
+            }
+            deleteNode = currentNode;
+            currentNode.prev.next = currentNode.next;
+            currentNode.next.prev = currentNode.prev;
         }
-        else {
-           for(let i=0; i < index-1; i++) {
-            currentNode = currentNode.next;
-           }
-           let deleteNode = currentNode.next;
-           currentNode.next = currentNode.next.next;
-           this.count--;
-           return deleteNode;
-        }
-    }
-    deleteLast() {
-        this.deleteAt(this.count-1);
+
+        this.count--;
+        return deleteNode;
     }
 
+    // 맨 뒤 노드를 삭제하고 반환
+    deleteLast() {
+        return this.deleteAt(this.count - 1);
+    }
+
+    // 특정 인덱스 위치의 노드의 데이터를 반환
     getNodeAt(index) {
-        if(index > this.count || index < 0) {
+        if (index >= this.count || index < 0) {
             throw new Error("범위를 넘어갔습니다.");
         }
         let currentNode = this.head;
-
-        for(let i=0; i < index; i++) {
+        for (let i = 0; i < index; i++) {
             currentNode = currentNode.next;
         }
-
         return currentNode.data;
+    }
+
+    // 전체 리스트 내용을 출력
+    printAll() {
+        let currentNode = this.head;
+        let text = "[";
+        while (currentNode !== null) {
+            text += currentNode.data;
+            currentNode = currentNode.next;
+            if (currentNode !== null) {
+                text += ", ";
+            }
+        }
+        text += "]";
+        console.log(text);
     }
 }
 
-export {Node, LinkedList};
+export { Node, DoubleLinkedList };
--- deque.mjs
+++ deque.mjs
@@ -1,31 +1,42 @@
-import { DoubleLinkedList } from "./DoubleLinkedList.mjs" 
+// DoubleLinkedList 클래스를 가져옴 (이중 연결 리스트 사용)
+import { DoubleLinkedList } from "./DoubleLinkedList.mjs";
 
 class Deque {
     constructor() {
+        // 내부적으로 DoubleLinkedList를 사용하여 양쪽에서 삽입/삭제가 가능하게 함
         this.list = new DoubleLinkedList();
     }
 
+    // 모든 요소 출력 (DoubleLinkedList의 printAll을 호출)
     printAll() {
         this.list.printAll();
     }
 
-    addFirst() {
-        this.list.insertAt(0, data);
+    // 앞쪽(헤드)에 요소 추가
+    addFirst(data) {
+        this.list.insertAt(0, data); // 0번째 위치에 삽입
     }
 
+    // 앞쪽(헤드) 요소 제거 후 반환
     removeFirst() {
-        return this.list.deleteAt(0);
+        return this.list.deleteAt(0); // 0번째 요소 삭제
     }
 
-    addLast() {
-        this.list.insertLast();
+    // 뒤쪽(테일)에 요소 추가
+    addLast(data) {
+        this.list.insertLast(data); // 마지막에 삽입
     }
 
+    // 뒤쪽(테일) 요소 제거 후 반환
     removeLast() {
-        this.list.deleteLast();
+        return this.list.deleteLast(); // 마지막 요소 삭제
     }
 
+    // Deque가 비어 있는지 여부 반환
     isEmpty() {
-        return (this.list.count === 0);
+        return this.list.count === 0; // 요소 개수가 0이면 true
     }
 }
+
+// Deque 클래스를 다른 파일에서 사용할 수 있도록 export
+export { Deque };
